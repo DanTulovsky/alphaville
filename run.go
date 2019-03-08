61425f66b5e4984de1d784a6f452e555498ba9fe
Dan Tulovsky <dant@wetsnow.com>
2019-03-08 14:01:12 -0500

    updates

--- run.go
+++ run.go
@@ -11,6 +11,7 @@ import (
 	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
 	"github.com/google/uuid"
+	"gogs.wetsnow.com/dant/alphaville/utils"
 	"golang.org/x/image/colornames"
 )
 
@@ -34,183 +35,199 @@ type world struct {
 
 // object is an object in the world
 type object struct {
-	name string
-	id   uuid.UUID
-	// this is the location of the object in the world
-	rect  pixel.Rect
+	name  string
+	id    uuid.UUID
 	color color.Color
-	imd   *imdraw.IMDraw
-	// size of the object
+
+	// initial speed and mass of object
+	speed float64 // horizontal speed (negative means move left)
+	mass  float64
+
+	// size of the object, assuming it's a rectangle
 	W, H float64
+
+	// draws the object
+	imd *imdraw.IMDraw
+
 	// initial location of the object (bottom left corner)
 	iX, iY float64
+
+	// physics properties of the object
+	phys *objectPhys
+}
+
+type objectPhys struct {
+
 	// current horizontal and vertical speed of object
 	vel pixel.Vec
+
 	// currentMass of the object
 	currentMass float64
-	// originalMass is the original object mass
-	originalMass float64
+
+	// this is the location of the object in the world
+	rect pixel.Rect
 }
 
 // update the object every frame
 func (o *object) update(w *world) {
 	// if above ground, fall based on mass and gravity
-	if o.rect.Min.Y > w.ground.rect.Max.Y {
+	if o.phys.rect.Min.Y > w.ground.phys.rect.Max.Y {
 		// more massive objects fall faster
-		o.vel.Y = w.gravity * o.currentMass
+		o.phys.vel.Y = w.gravity * o.phys.currentMass
 	}
 
 	// if mass is 0, rise based on gravity
-	if o.currentMass == 0 {
-		o.vel.Y = -1 * w.gravity
+	if o.phys.currentMass == 0 {
+		o.phys.vel.Y = -1 * w.gravity
 	}
 
 	// fall
-	if o.vel.Y < 0 {
+	if o.phys.vel.Y < 0 {
 		switch {
-		case o.rect.Min.Y+o.vel.Y < w.ground.rect.Max.Y:
+		case o.phys.rect.Min.Y+o.phys.vel.Y < w.ground.phys.rect.Max.Y:
 			// would fall below ground
-			o.rect = o.rect.Moved(pixel.V(0, w.ground.rect.Max.Y-o.rect.Min.Y))
-			o.vel.Y = 0
+			o.phys.rect = o.phys.rect.Moved(pixel.V(0, w.ground.phys.rect.Max.Y-o.phys.rect.Min.Y))
+			o.phys.vel.Y = 0
 		default:
 			// if about to fall on another, rise back up
 			for _, other := range w.objects {
 				if o.id == other.id {
 					continue // skip yourself
 				}
-				if o.rect.Max.X < other.rect.Min.X || o.rect.Min.X > other.rect.Max.X {
+				if o.phys.rect.Max.X < other.phys.rect.Min.X || o.phys.rect.Min.X > other.phys.rect.Max.X {
 					continue // no intersection in X axis
 				}
 
-				gap := o.rect.Min.Y - other.rect.Max.Y
-				if !(gap >= 0 && o.rect.Min.Y+o.vel.Y-other.rect.Max.Y <= 0) {
+				gap := o.phys.rect.Min.Y - other.phys.rect.Max.Y
+				if !(gap >= 0 && o.phys.rect.Min.Y+o.phys.vel.Y-other.phys.rect.Max.Y <= 0) {
 					// too far apart
 					continue
 				}
 
 				// if about to hit another one
 				switch {
-				case other.vel.Y < 0: // falling also
-					if math.Abs(o.vel.Y) > math.Abs(other.vel.Y) {
+				case other.phys.vel.Y < 0: // falling also
+					if math.Abs(o.phys.vel.Y) > math.Abs(other.phys.vel.Y) {
 						// close and falling faster than what is below
-						o.currentMass = 0
-						o.vel.Y = 0
+						o.phys.currentMass = 0
+						o.phys.vel.Y = 0
 						return
 
 					}
-				case other.rect.Min.Y == w.ground.rect.Max.Y:
+				case other.phys.rect.Min.Y == w.ground.phys.rect.Max.Y:
 					// close and falling on something on the ground
-					o.currentMass = 0
-					o.vel.Y = 0
+					o.phys.currentMass = 0
+					o.phys.vel.Y = 0
 					return
-				case other.rect.Min.Y > 0: // rising
-					o.currentMass = 0
-					o.vel.Y = 0
+				case other.phys.rect.Min.Y > 0: // rising
+					o.phys.currentMass = 0
+					o.phys.vel.Y = 0
 					return
 
 				}
 			}
-			o.rect = o.rect.Moved(pixel.V(0, o.vel.Y))
+			o.phys.rect = o.phys.rect.Moved(pixel.V(0, o.phys.vel.Y))
 		}
 		return
 	}
 
 	// rise
-	if o.vel.Y > 0 {
+	if o.phys.vel.Y > 0 {
 		switch {
-		case o.rect.Max.Y+o.vel.Y > w.Y:
+		case o.phys.rect.Max.Y+o.phys.vel.Y > w.Y:
 			// would rise above ceiling
-			o.rect = o.rect.Moved(pixel.V(0, w.Y-o.rect.Max.Y))
-			o.vel.Y = 0
-			o.currentMass = o.originalMass
+			o.phys.rect = o.phys.rect.Moved(pixel.V(0, w.Y-o.phys.rect.Max.Y))
+			o.phys.vel.Y = 0
+			o.phys.currentMass = o.mass
 		default:
 			for _, other := range w.objects {
 				if o.id == other.id {
 					continue // skip yourself
 				}
-				if o.rect.Max.X < other.rect.Min.X || o.rect.Min.X > other.rect.Max.X {
+				if o.phys.rect.Max.X < other.phys.rect.Min.X || o.phys.rect.Min.X > other.phys.rect.Max.X {
 					continue // no intersection in X axis
 				}
-				gap := other.rect.Min.Y - o.rect.Max.Y
+				gap := other.phys.rect.Min.Y - o.phys.rect.Max.Y
 				if gap < 0 {
 					continue
 				}
 				// if about to hit another one
-				if other.rect.Min.Y-(o.rect.Max.Y+o.vel.Y) <= o.vel.Y {
-					o.currentMass = o.originalMass
-					o.vel.Y = 0
+				if other.phys.rect.Min.Y-(o.phys.rect.Max.Y+o.phys.vel.Y) <= o.phys.vel.Y {
+					o.phys.currentMass = o.mass
+					o.phys.vel.Y = 0
 					return
 				}
 			}
-			o.rect = o.rect.Moved(pixel.V(0, o.vel.Y))
-
+			o.phys.rect = o.phys.rect.Moved(pixel.V(0, o.phys.vel.Y))
 		}
 		return
 	}
 
 	// jump back up with random probability by setting mass to 0
 	// if utils.RandomInt(0, 1000) < 1 {
-	// 	o.currentMass = 0 // make it float
+	// 	o.phys.currentMass = 0 // make it float
 	// 	return
 	// }
 
 	// move if on the ground
-	if o.rect.Min.X <= 0 {
-		o.vel.X = math.Abs(o.vel.X)
+
+	// switch directions of at the end of screen
+	if o.phys.rect.Min.X <= 0 {
+		o.phys.vel.X = math.Abs(o.phys.vel.X)
 	}
-	if o.rect.Max.X >= w.X {
-		o.vel.X = -1 * math.Abs(o.vel.X)
+	if o.phys.rect.Max.X >= w.X {
+		o.phys.vel.X = -1 * math.Abs(o.phys.vel.X)
 	}
 
 	// if about to bump into another object, rise up
 	switch {
-	case o.vel.X > 0: // moving right
+	case o.phys.vel.X > 0: // moving right
 		for _, other := range w.objects {
 			if o.id == other.id {
 				continue // skip yourself
 			}
-			if other.rect.Min.Y > o.rect.Max.Y {
+			if other.phys.rect.Min.Y > o.phys.rect.Max.Y {
 				continue // ignore falling objects higher than you
 			}
 
 			// velocity of the other object (may be 0 if it's coming down)
 			var otherVel float64
-			if other.rect.Min.Y < o.rect.Max.Y {
+			if other.phys.rect.Min.Y < w.ground.phys.rect.Max.Y {
 				otherVel = 0
 			} else {
-				otherVel = other.vel.X
+				otherVel = other.phys.vel.X
 			}
-			if o.rect.Max.X < other.rect.Min.X && o.rect.Max.X+o.vel.X > other.rect.Min.X+otherVel {
-				o.currentMass = 0
+			if o.phys.rect.Max.X < other.phys.rect.Min.X && o.phys.rect.Max.X+o.phys.vel.X >= other.phys.rect.Min.X+otherVel {
+				o.phys.currentMass = 0
 				return
 			}
 		}
-	case o.vel.X < 0: // moving left
+	case o.phys.vel.X < 0: // moving left
 		for _, other := range w.objects {
 			if o.id == other.id {
 				continue // skip yourself
 			}
-			if other.rect.Min.Y > o.rect.Max.Y {
+			if other.phys.rect.Min.Y > o.phys.rect.Max.Y {
 				continue // ignore falling objects higher than you
 			}
 			// velocity of the other object (may be 0 if it's coming down)
 			var otherVel float64
-			if other.rect.Min.Y < o.rect.Max.Y {
+			if other.phys.rect.Min.Y < w.ground.phys.rect.Max.Y {
 				otherVel = 0
 			} else {
-				otherVel = other.vel.X
+				otherVel = other.phys.vel.X
 			}
-			if o.rect.Min.X > other.rect.Max.X && o.rect.Min.X+o.vel.X < other.rect.Max.X+otherVel {
-				o.currentMass = 0
+			if o.phys.rect.Min.X > other.phys.rect.Max.X && o.phys.rect.Min.X+o.phys.vel.X <= other.phys.rect.Max.X+otherVel {
+				o.phys.currentMass = 0
 				return
 			}
 		}
 	}
 	// if utils.RandomInt(0, 1000) > 1 {
-	// 	o.currentMass = 0
+	// 	o.phys.currentMass = 0
 	// }
 	// move
-	o.rect = o.rect.Moved(pixel.V(o.vel.X, 0))
+	o.phys.rect = o.phys.rect.Moved(pixel.V(o.phys.vel.X, 0))
 
 }
 
@@ -218,8 +235,8 @@ func (o *object) draw(win *pixelgl.Window) {
 	o.imd.Clear()
 	o.imd.Reset()
 	o.imd.Color = o.color
-	o.imd.Push(o.rect.Min)
-	o.imd.Push(o.rect.Max)
+	o.imd.Push(o.phys.rect.Min)
+	o.imd.Push(o.phys.rect.Max)
 	o.imd.Rectangle(0)
 	o.imd.Draw(win)
 }
@@ -250,139 +267,124 @@ func populate(w *world) {
 
 	objs := []*object{
 		{
-			name:         "one",
-			id:           uuid.New(),
-			color:        colornames.Red,
-			imd:          imdraw.New(nil),
-			W:            60,
-			H:            60,
-			vel:          pixel.V(1, 0),
-			originalMass: 2,
-			currentMass:  2,
-			iX:           200,
-			iY:           700,
+			name:  "one",
+			id:    uuid.New(),
+			color: colornames.Red,
+			imd:   imdraw.New(nil),
+			W:     60,
+			H:     60,
+			speed: 1,
+			mass:  3,
+			phys:  &objectPhys{},
 		},
 		{
-			name:         "two",
-			id:           uuid.New(),
-			color:        colornames.Burlywood,
-			imd:          imdraw.New(nil),
-			W:            60,
-			H:            60,
-			vel:          pixel.V(2, 0),
-			originalMass: 1,
-			currentMass:  1,
-			iX:           200,
-			iY:           500,
+			name:  "two",
+			id:    uuid.New(),
+			color: colornames.Burlywood,
+			imd:   imdraw.New(nil),
+			W:     60,
+			H:     60,
+			speed: 1,
+			mass:  2,
+			phys:  &objectPhys{},
 		},
 		{
-			name:         "three",
-			id:           uuid.New(),
-			color:        colornames.Yellow,
-			imd:          imdraw.New(nil),
-			W:            60,
-			H:            60,
-			vel:          pixel.V(1.12, 0),
-			originalMass: 0.5,
-			currentMass:  0.5,
-			iX:           200,
-			iY:           700,
+			name:  "three",
+			id:    uuid.New(),
+			color: colornames.Yellow,
+			imd:   imdraw.New(nil),
+			W:     60,
+			H:     60,
+			speed: 1.12,
+			mass:  0.5,
+			phys:  &objectPhys{},
 		},
 		{
-			name:         "four",
-			id:           uuid.New(),
-			color:        colornames.Green,
-			imd:          imdraw.New(nil),
-			W:            80,
-			H:            80,
-			vel:          pixel.V(.12, 0),
-			originalMass: 0.1,
-			currentMass:  0.1,
-			iX:           400,
-			iY:           700,
+			name:  "four",
+			id:    uuid.New(),
+			color: colornames.Green,
+			imd:   imdraw.New(nil),
+			W:     80,
+			H:     80,
+			speed: 0.12,
+			mass:  0.1,
+			phys:  &objectPhys{},
 		},
 		{
-			name:         "four",
-			id:           uuid.New(),
-			color:        colornames.Green,
-			imd:          imdraw.New(nil),
-			W:            80,
-			H:            80,
-			vel:          pixel.V(.12, 0),
-			originalMass: 0.2,
-			currentMass:  0.2,
-			iX:           500,
-			iY:           700,
+			name:  "five",
+			id:    uuid.New(),
+			color: colornames.Chartreuse,
+			imd:   imdraw.New(nil),
+			W:     80,
+			H:     80,
+			speed: 1.12,
+			mass:  4.2,
+			phys:  &objectPhys{},
 		},
 		{
-			name:         "five",
-			id:           uuid.New(),
-			color:        colornames.Magenta,
-			imd:          imdraw.New(nil),
-			W:            20,
-			H:            80,
-			vel:          pixel.V(1.2, 0),
-			originalMass: 4.2,
-			currentMass:  4.2,
-			iX:           500,
-			iY:           700,
+			name:  "six",
+			id:    uuid.New(),
+			color: colornames.Violet,
+			imd:   imdraw.New(nil),
+			W:     90,
+			H:     20,
+			speed: 1.5,
+			mass:  2.2,
+			phys:  &objectPhys{},
 		},
 		{
-			name:         "six",
-			id:           uuid.New(),
-			color:        colornames.Violet,
-			imd:          imdraw.New(nil),
-			W:            90,
-			H:            20,
-			vel:          pixel.V(1.5, 0),
-			originalMass: 2.2,
-			currentMass:  2.2,
-			iX:           200,
-			iY:           500,
+			name:  "seven",
+			id:    uuid.New(),
+			color: colornames.Teal,
+			imd:   imdraw.New(nil),
+			W:     30,
+			H:     70,
+			speed: 0.5,
+			mass:  12,
+			phys:  &objectPhys{},
 		},
 		{
-			name:         "seven",
-			id:           uuid.New(),
-			color:        colornames.Teal,
-			imd:          imdraw.New(nil),
-			W:            30,
-			H:            70,
-			vel:          pixel.V(.5, 0),
-			originalMass: 12,
-			currentMass:  12,
-			iX:           800,
-			iY:           500,
+			name:  "eight",
+			id:    uuid.New(),
+			color: colornames.Whitesmoke,
+			imd:   imdraw.New(nil),
+			W:     300,
+			H:     70,
+			speed: 0.1,
+			mass:  2,
+			phys:  &objectPhys{},
 		},
 		{
-			name:         "eight",
-			id:           uuid.New(),
-			color:        colornames.Whitesmoke,
-			imd:          imdraw.New(nil),
-			W:            300,
-			H:            70,
-			vel:          pixel.V(.1, 0),
-			originalMass: 2,
-			currentMass:  2,
-			iX:           300,
-			iY:           500,
-		},
-		{
-			name:         "nine",
-			id:           uuid.New(),
-			color:        colornames.Tomato,
-			imd:          imdraw.New(nil),
-			W:            10,
-			H:            15,
-			vel:          pixel.V(11, 0),
-			originalMass: .2,
-			currentMass:  .2,
-			iX:           30,
-			iY:           50,
+			name:  "nine",
+			id:    uuid.New(),
+			color: colornames.Tomato,
+			imd:   imdraw.New(nil),
+			W:     10,
+			H:     15,
+			speed: 11,
+			mass:  .2,
+			phys:  &objectPhys{},
 		},
 	}
 
 	for _, o := range objs {
-		o.rect = pixel.R(o.iX, o.iY, o.W+o.iX, o.H+o.iY)
+		if o.iY == 0 {
+			// place randomly
+			o.iY = utils.RandomFloat64(w.ground.phys.rect.Max.Y, w.Y-o.H)
+		}
+		if o.iX == 0 {
+			// place randomly
+			o.iX = utils.RandomFloat64(0, w.Y-o.W)
+		}
+		// set bounding rectangle based on size and location
+		o.phys.rect = pixel.R(o.iX, o.iY, o.W+o.iX, o.H+o.iY)
+
+		// set velocity vector
+		o.phys.vel = pixel.V(o.speed, 0)
+
+		// set current mass based on initial mass
+		o.phys.currentMass = o.mass
+
 		w.objects = append(w.objects, o)
 	}
 
@@ -395,8 +397,9 @@ func run() {
 		imd:   imdraw.New(nil),
 		W:     1024,
 		H:     40,
+		phys:  &objectPhys{},
 	}
-	ground.rect = pixel.R(0, 0, 1024, 40)
+	ground.phys.rect = pixel.R(0, 0, 1024, 40)
 
 	world := &world{
 		objects: []*object{},
