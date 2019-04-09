7408d4c3227a15eeb06ac629939817c83c5c4d6e
Dan Tulovsky <dtulovsky@google.com>
2019-04-08 21:20:35 -0400

    updates

--- playground/playground.go
+++ playground/playground.go
@@ -28,20 +28,22 @@ func run() {
 		target,
 		pixel.R(0, 0, 10, 10),
 		pixel.R(20, 20, 30, 30),
+		pixel.R(20, 400, 40, 600),
 		pixel.R(10, 40, 50, 50),
 		pixel.R(100, 40, 150, 50),
 		pixel.R(0, 400, 500, 405),
 		pixel.R(511, 400, 800, 405),
-		pixel.R(0, 200, 100, 205),
 		pixel.R(111, 200, 800, 205),
-		pixel.R(650, 405, 670, 1130),
-		// pixel.R(650, 525, 670, 525),
-		// pixel.R(100, 100, 150, 280),
-		// pixel.R(500, 500, 650, 780),
+		pixel.R(650, 405, 670, 760),
+		pixel.R(650, 525, 670, 525),
+		pixel.R(100, 100, 150, 280),
+		pixel.R(500, 500, 650, 780),
+		pixel.R(100, 100, 200, 200),
+		pixel.R(150, 150, 250, 250),
 	}
 
 	var width float64 = 800
-	var height float64 = 1200
+	var height float64 = 860
 
 	bounds := pixel.R(0, 0, width, height)
 
@@ -167,12 +169,12 @@ func draw(win *pixelgl.Window, qt *quadtree.Tree, g *graph.Graph, atlas *text.At
 		imd.Rectangle(1)
 		imd.Draw(win)
 
-		txt := text.New(r.Bounds().Center(), atlas)
-		txt.Color = colornames.Darkgray
-		label := fmt.Sprintf("%v,\n%v", r.Bounds().Center().X, r.Bounds().Center().Y)
-		txt.Dot.X -= txt.BoundsOf(label).W() / 2
-		fmt.Fprintf(txt, "%v", label)
-		txt.Draw(win, pixel.IM)
+		// txt := text.New(r.Bounds().Center(), atlas)
+		// txt.Color = colornames.Darkgray
+		// label := fmt.Sprintf("%v,\n%v", r.Bounds().Center().X, r.Bounds().Center().Y)
+		// txt.Dot.X -= txt.BoundsOf(label).W() / 2
+		// fmt.Fprintf(txt, "%v", label)
+		// txt.Draw(win, pixel.IM)
 	}
 
 	imd = imdraw.New(nil)
